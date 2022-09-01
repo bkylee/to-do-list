42b052d5c1a6bdd89e58448fbdd7fe929cc9eca6
bkylee <brian.ky.lee@outlook.com>
2022-09-01 19:32:20 -0400

    added sourcemap script in package

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry:{
         index: './src/index.js',
     },
+    devtool: 'inline-source-map',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
