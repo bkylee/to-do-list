30a2b3071787b9c4a2ef28b54fde9b34e409c7ee
bkylee <brian.ky.lee@outlook.com>
2022-09-01 19:26:33 -0400

    Set up html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry:'./src/index.js',
+    entry:{
+        index: './src/index.js',
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To-Do App',
+        }),
+    ],
     module: {
         rules:[
             {
