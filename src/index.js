cd71f61aa800cd8b80c7384b77d7e916cc41f9a5
bkylee <brian.ky.lee@outlook.com>
2022-09-02 18:49:53 -0400

    Created header stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,49 @@
 //Creating the elements in HTML 
 
-const header = (()=>{
-    const search = document.createElement('input');
+import './style.css';
+import cap from './cap.png';
 
+const header = (()=>{
+    const wrapper = document.createElement('div');
+    document.body.appendChild(wrapper);
+    wrapper.setAttribute('id', 'header');
+
+
+
+
+    wrapper.appendChild(searchBar());
+    wrapper.appendChild(profile());
+    wrapper.appendChild(settings());
 })();
 
+
+//function that returns the search bar; 
+function searchBar() {
+    const search = document.createElement('input');
+    search.setAttribute('type', 'text');
+    search.setAttribute('id', 'search');
+
+    const searchLabel = document.createElement('label');
+    searchLabel.setAttribute('id', 'search');
+    searchLabel.textContent = "Search:";
+
+    searchLabel.appendChild(search);
+    
+    return searchLabel; 
+};
+
+function profile() {
+    const pic = new Image();
+    pic.src = cap; 
+    pic.width = 25;
+    pic.height = 25;
+
+    return pic;
+};
+
+function settings() { 
+    const settings = document.createElement('div');
+    settings.textContent = '⚙️';
+
+    return settings; 
+}
